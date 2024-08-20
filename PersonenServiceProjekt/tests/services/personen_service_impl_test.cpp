3e9373270547d84098110cb825b639d8d099b60a
Jo Wagner <jowagner@limago.de>
2024-08-20 16:35:32 +0200

    initial commit

--- PersonenServiceProjekt/tests/services/personen_service_impl_test.cpp
+++ PersonenServiceProjekt/tests/services/personen_service_impl_test.cpp
@@ -4,6 +4,16 @@
 
 #include "personen_service_impl_test.h"
 
+TEST_P(personen_service_impl_parameter_test, speichern__throws_personen_service_exception) {
+    try {
+        EXPECT_CALL(repoMock, save_or_update(_)).Times(0);
+        objectUnderTest.speichern(invalidPerson);
+        FAIL() << "Exception erwartet";
+    } catch (personen_service_exception &ex) {
+        EXPECT_STREQ(ex.what(), expectedErrorMessage.c_str());
+    }
+}
+
 TEST_F(personen_service_impl_test,speichern__vorname_zu_kurz__throws_personen_service_exception){
     try {
         // Arrange
@@ -89,3 +99,14 @@ TEST_F(personen_service_impl_test, speichern__happy_day__person_passed_to_repo_o
 void personen_service_impl_test::SetUp() {
     ON_CALL(blacklistMock, is_blacklisted(_)).WillByDefault(Return(false));
 }
+
+INSTANTIATE_TEST_SUITE_P(
+        speichern_invalid_names, // Name der Testa frei waehlbar
+        personen_service_impl_parameter_test, // Verbindung zur Testklasse
+        Values(
+                std::make_pair(person{"","Doe"},"Vorname zu kurz" ),
+                std::make_pair(person{"J","Doe"},"Vorname zu kurz" ),
+                std::make_pair(person{"John",""},"Nachname zu kurz" ),
+                std::make_pair(person{"John","D"},"Nachname zu kurz" )
+        )
+);
